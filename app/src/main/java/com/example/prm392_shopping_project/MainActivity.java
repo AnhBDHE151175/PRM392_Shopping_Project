9da32201ed6b335cbf950cdfd4ff6af63407b12b
ANHBDHE151175 <ANHBDHE151175@fpt.edu.vn>
2022-10-12 23:36:44 +0700

    Create view

--- app/src/main/java/com/example/prm392_shopping_project/MainActivity.java
+++ app/src/main/java/com/example/prm392_shopping_project/MainActivity.java
@@ -2,13 +2,114 @@ package com.example.prm392_shopping_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.prm392_shopping_project.adapter.CategoryAdapter;
+import com.example.prm392_shopping_project.adapter.DiscountedProductAdapter;
+import com.example.prm392_shopping_project.adapter.RecentlyViewedAdapter;
+import com.example.prm392_shopping_project.model.Category;
+import com.example.prm392_shopping_project.model.DiscountedProducts;
+import com.example.prm392_shopping_project.model.RecentlyViewed;
+import static com.example.prm392_shopping_project.R.drawable.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView discountRecyclerView, categoryRecyclerView, recentlyViewedRecycler;
+    DiscountedProductAdapter discountedProductAdapter;
+    List<DiscountedProducts> discountedProductsList;
+
+    CategoryAdapter categoryAdapter;
+    List<Category> categoryList;
+
+    RecentlyViewedAdapter recentlyViewedAdapter;
+    List<RecentlyViewed> recentlyViewedList;
+
+    TextView allCategory;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        discountRecyclerView = findViewById(R.id.discountedRecycler);
+        categoryRecyclerView = findViewById(R.id.categoryRecycler);
+        allCategory = findViewById(R.id.allCategoryImage);
+        recentlyViewedRecycler = findViewById(R.id.recently_item);
+
+
+        allCategory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, AllCategory.class);
+                startActivity(i);
+            }
+        });
+
+        // adding data to model
+        discountedProductsList = new ArrayList<>();
+        discountedProductsList.add(new DiscountedProducts(1, discountberry));
+        discountedProductsList.add(new DiscountedProducts(2, discountbrocoli));
+        discountedProductsList.add(new DiscountedProducts(3, discountmeat));
+        discountedProductsList.add(new DiscountedProducts(4, discountberry));
+        discountedProductsList.add(new DiscountedProducts(5, discountbrocoli));
+        discountedProductsList.add(new DiscountedProducts(6, discountmeat));
+
+        // adding data to model
+        categoryList = new ArrayList<>();
+        categoryList.add(new Category(1, ic_home_fruits));
+        categoryList.add(new Category(2, ic_home_fish));
+        categoryList.add(new Category(3, ic_home_meats));
+        categoryList.add(new Category(4, ic_home_veggies));
+        categoryList.add(new Category(5, ic_home_fruits));
+        categoryList.add(new Category(6, ic_home_fish));
+        categoryList.add(new Category(7, ic_home_meats));
+        categoryList.add(new Category(8, ic_home_veggies));
+
+        // adding data to model
+        recentlyViewedList = new ArrayList<>();
+        recentlyViewedList.add(new RecentlyViewed("Watermelon", "Watermelon has high water content and also provides some fiber.", "₹ 80", "1", "KG", card4, b4));
+        recentlyViewedList.add(new RecentlyViewed("Papaya", "Papayas are spherical or pear-shaped fruits that can be as long as 20 inches.", "₹ 85", "1", "KG", card3, b3));
+        recentlyViewedList.add(new RecentlyViewed("Strawberry", "The strawberry is a highly nutritious fruit, loaded with vitamin C.", "₹ 30", "1", "KG", card2, b1));
+        recentlyViewedList.add(new RecentlyViewed("Kiwi", "Full of nutrients like vitamin C, vitamin K, vitamin E, folate, and potassium.", "₹ 30", "1", "PC", card1, b2));
+
+        setDiscountedRecycler(discountedProductsList);
+        setCategoryRecycler(categoryList);
+        setRecentlyViewedRecycler(recentlyViewedList);
+
+    }
+
+    private void setDiscountedRecycler(List<DiscountedProducts> dataList) {
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        discountRecyclerView.setLayoutManager(layoutManager);
+        discountedProductAdapter = new DiscountedProductAdapter(this,dataList);
+        discountRecyclerView.setAdapter(discountedProductAdapter);
+    }
+
+
+    private void setCategoryRecycler(List<Category> categoryDataList) {
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        categoryRecyclerView.setLayoutManager(layoutManager);
+        categoryAdapter = new CategoryAdapter(this,categoryDataList);
+        categoryRecyclerView.setAdapter(categoryAdapter);
+    }
+
+    private void setRecentlyViewedRecycler(List<RecentlyViewed> recentlyViewedDataList) {
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        recentlyViewedRecycler.setLayoutManager(layoutManager);
+        recentlyViewedAdapter = new RecentlyViewedAdapter(this,recentlyViewedDataList);
+        recentlyViewedRecycler.setAdapter(recentlyViewedAdapter);
     }
 }
